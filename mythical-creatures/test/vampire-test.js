8dadd2eec6c974647681067ec2247ee34b7e4972
Jani Palomino <palomino.jani@gmail.com>
2021-12-21 00:01:08 -0700

    Add commented noted in test file

--- mythical-creatures/test/vampire-test.js
+++ mythical-creatures/test/vampire-test.js
@@ -9,57 +9,58 @@ describe('Vampire', function() {
 
   it('should instantiate our good friend, Vampire', function() {
     var dracula = new Vampire();
-
+//declaring a new instance
     assert.instanceOf(dracula, Vampire);
   });
 
   it('should have a name', function() {
     var vampire = new Vampire('Jhun');
-
+  //new param of name
     assert.equal(vampire.name, 'Jhun');
   });
 
   it('should have a pet bat as a default', function() {
     var vampire = new Vampire('Brittany');
-
+    //new param of pet
     assert.equal(vampire.pet, 'bat');
   });
 
   it('should take an argument for pet', function() {
     var vampire = new Vampire('Jeff', 'fox');
-    var vampyre = new Vampire('David', 'armadillo');
-
+    var vampire = new Vampire('David', 'armadillo');
+    //pet argument, or default to 'bat'
     assert.equal(vampire.pet, 'fox');
-    assert.equal(vampyre.pet, 'armadillo');
+    assert.equal(vampire.pet, 'armadillo');
   });
 
   it('should be thirsty', function() {
     var vampire = new Vampire('Andy');
-
+  //new key of thirsty, with a value of true
     assert.equal(vampire.thirsty, true);
   });
 
   it('should not be thirsty if vampire has drank', function() {
     var vampire = new Vampire('Meeka');
-
+    //invoking a method of drink
     vampire.drink();
-
+    //this.thirsty = false;
     assert.equal(vampire.thirsty, false);
   });
 
   it('should be start with no ounces of blood drank', function() {
     var vampire = new Vampire('Bobby');
-
+    //new key with value of 0
+        // console.log(vampire.ouncesDrank);
     assert.equal(vampire.ouncesDrank, 0);
   });
 
   it('should drink 10 ounces of blood at a time', function() {
     var vampire = new Vampire('Margot');
-
+    //calling the drink method 3 times, each time is 10
     vampire.drink();
     vampire.drink();
     vampire.drink();
-
+    // console.log(vampire.ouncesDrank);
     assert.equal(vampire.ouncesDrank, 30);
   });
 
@@ -89,7 +90,7 @@ describe('Vampire', function() {
     vampire.drink();
 
     assert.equal(vampire.ouncesDrank, 50);
-
+  //calling for an if conditional
     assert.equal(vampire.drink(), 'I\'m too full to drink anymore!');
   });
 });
