7f320b48801e80b8a38c98f35a682aa215ea1070
Jani Palomino <palomino.jani@gmail.com>
2021-12-21 00:52:46 -0700

    Unskip tests, complete class, and pass all tests

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,28 @@
 class Runner {
+  constructor (name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
 
+  runSomeMiles(miles) {
+    this.milesRun += miles;
+    this.fitness += miles;
+  }
+
+  stretch() {
+    this.fitness += .5;
+  }
+
+  runRace(raceName, miles) {
+    this.completedRaces.push(raceName);
+    this.runSomeMiles(miles);
+      //OR we can do
+      //this.milesRun += miles;
+      //this.fitness += miles;
+  }
 }
 
 module.exports = Runner;
