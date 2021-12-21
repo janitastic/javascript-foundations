cc5995535a4928890b3cb5fffa58fb2a793969e7
Jani Palomino <palomino.jani@gmail.com>
2021-12-21 00:50:44 -0700

    Unskip tests, complete class, and pass all tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,25 @@
 class Craft {
+  constructor(craft, type, materials) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+    this.totalCost = 0;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`;
+  }
+
+  calculateProjectTotal() {
+    for (var i = 0; i < this.materials.length; i++) {
+      var costPerCraft = this.materials[i].calculateMaterialCost();
+      // console.log(costPerCraft);
+      // console.log(this.totalCost);
+      this.totalCost += costPerCraft;
+    }
+    return this.totalCost;
+  }
 }
 
 module.exports = Craft;
